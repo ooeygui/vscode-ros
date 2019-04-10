d524f6ccc37752936c3aedb3d2c8ec6abee8134f
James Xu <kejxu@microsoft.com>
2019-04-10 11:14:46 -0700

    add rosrun and roslaunch as commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,15 @@
+import * as path from "path";
+import * as vscode from "vscode";
+
 import * as build from "./build";
 import * as catkin from "./catkin";
 import CatkinTaskProvider from "./catkin-task-provider";
-import * as constants from "./constants";
 import CppFormatter from "./cpp-formatter";
 import * as debug from "./debug";
 import * as master from "./master";
 import * as pfs from "./promise-fs";
 import * as utils from "./utils";
-import * as path from "path";
-import * as vscode from "vscode";
+import * as platformHelper from "./platformHelper"
 
 /**
  * The catkin workspace base dir.
@@ -38,6 +39,19 @@ export let onDidChangeEnv = onEnvChanged.event;
  * Subscriptions to dispose when the environment is changed.
  */
 let subscriptions = <vscode.Disposable[]>[];
+
+export enum RosCommands {
+    CreateCatkinPackage = "ros.createCatkinPackage",
+    CreateTerminal = "ros.createTerminal",
+    GetDebugSettings = "ros.getDebugSettings",
+    Rosrun = "ros.rosrun",
+    Roslaunch = "ros.roslaunch",
+    ShowMasterStatus = "ros.showMasterStatus",
+    StartRosCore = "ros.startCore",
+    TerminateRosCore = "ros.stopCore",
+    UpdateCppProperties = "ros.updateCppProperties",
+    UpdatePythonPath = "ros.updatePythonPath",
+}
 
 export async function activate(context: vscode.ExtensionContext) {
     // Activate if we're in a catkin workspace.
@@ -73,6 +87,8 @@ export async function activate(context: vscode.ExtensionContext) {
     }));
 
     sourceRosAndWorkspace();
+
+    await platformHelper.performPlatformSetup();
 
     return {
         getBaseDir: () => baseDir,
@@ -132,20 +148,67 @@ function activateEnvironment(context: vscode.ExtensionContext) {
     subscriptions.push(vscode.workspace.registerTaskProvider("catkin", new CatkinTaskProvider()));
     subscriptions.push(vscode.debug.registerDebugConfigurationProvider("ros", new debug.RosDebugConfigProvider()));
 
-    // Register commands.
+    // register plugin commands
     subscriptions.push(
-        vscode.commands.registerCommand(constants.CMD_CREATE_CATKIN_PACKAGE, catkin.createPackage),
-        vscode.commands.registerCommand(constants.CMD_CREATE_TERMINAL, utils.createTerminal),
-        vscode.commands.registerCommand(constants.CMD_GET_DEBUG_SETTINGS, debug.getDebugSettings),
-        vscode.commands.registerCommand(constants.CMD_SHOW_MASTER_STATUS, master.showMasterStatus),
-        vscode.commands.registerCommand(constants.CMD_START_CORE, master.startCore),
-        vscode.commands.registerCommand(constants.CMD_STOP_CORE, () => master.stopCore(masterApi)),
-        vscode.commands.registerCommand(constants.CMD_UPDATE_CPP_PROPERTIES, build.updateCppProperties),
-        vscode.commands.registerCommand(constants.CMD_UPDATE_PYTHON_PATH, build.updatePythonPath),
+        vscode.commands.registerCommand(RosCommands.CreateCatkinPackage, catkin.createPackage),
+        vscode.commands.registerCommand(RosCommands.CreateTerminal, utils.createTerminal),
+        vscode.commands.registerCommand(RosCommands.GetDebugSettings, debug.getDebugSettings),
+        vscode.commands.registerCommand(RosCommands.ShowMasterStatus, master.showMasterStatus),
+        vscode.commands.registerCommand(RosCommands.StartRosCore, master.startCore),
+        vscode.commands.registerCommand(RosCommands.TerminateRosCore, () => master.stopCore(masterApi)),
+        vscode.commands.registerCommand(RosCommands.UpdateCppProperties, build.updateCppProperties),
+        vscode.commands.registerCommand(RosCommands.UpdatePythonPath, build.updatePythonPath),
+        vscode.commands.registerCommand(RosCommands.Rosrun, rosrundelegate),
+        vscode.commands.registerCommand(RosCommands.Roslaunch, roslaunchdelegate),
     );
 
     // Generate config files if they don't already exist.
     build.createConfigFiles();
+}
+
+async function rosrundelegate() {
+    // vscode.window.showInformationMessage("rosrun");
+    // let terminal = vscode.window.createTerminal({ name: "rosrun", env: env });
+
+    let terminal = await preparerosrun();
+    terminal.show();
+}
+
+async function preparerosrun(): Promise<vscode.Terminal> {
+    const packages = utils.getPackages();
+    const packageName = await vscode.window.showQuickPick(packages.then(Object.keys), { placeHolder: "Choose a package" });
+    if (packageName !== undefined) {
+        let basenames = (files: string[]) => files.map(file => path.basename(file));
+
+        const executables = utils.findPackageExecutables(packageName).then(basenames);
+        let target = await vscode.window.showQuickPick(executables, { placeHolder: "Choose an executable" });
+        let argument = await vscode.window.showInputBox({ placeHolder: "Enter any extra arguments" });
+        let terminal = vscode.window.createTerminal({ name: "rosrun", env: env });
+        terminal.sendText(`rosrun ${packageName} ${target} ${argument}`);
+        terminal.show();
+        return terminal;
+    } else {
+        // none of the packages selected, error!
+    }
+}
+
+async function roslaunchdelegate(): Promise<vscode.Terminal> {
+    const packages = utils.getPackages();
+    const packageName = await vscode.window.showQuickPick(packages.then(Object.keys), { placeHolder: "Choose a package" });
+    if (packageName !== undefined) {
+        let basenames = (files: string[]) => files.map(file => path.basename(file));
+
+        const launchFiles = await utils.findPackageLaunchFiles(packageName);
+        const launchFileBasenames = launchFiles.map((filename) => path.basename(filename));
+        let target = await vscode.window.showQuickPick(launchFileBasenames, { placeHolder: "Choose a launch file" });
+        let argument = await vscode.window.showInputBox({ placeHolder: "Enter any extra arguments" });
+        let terminal = vscode.window.createTerminal({ name: "roslaunch", env: env });
+        terminal.sendText(`roslaunch ${launchFiles[launchFileBasenames.indexOf(target)]} ${argument}`);
+        terminal.show();
+        return terminal;
+    } else {
+        // none of the packages selected, error!
+    }
 }
 
 /**
